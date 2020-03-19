74b86f5b288fe64160333c900bb89181f10f557d
Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2020-03-20 00:45:43 +0600

    go-env initialization add

--- src/main.go
+++ src/main.go
@@ -6,6 +6,12 @@ import (
 )
 
 func main() {
+	// Load environment variable from .env file
+	err := godotenv.Load()
+	if err != nil {
+		log.Printf("Error loading env file %v", err)
+	}
+
 	configuration := models.Configuration
 	router := startup.NewRouter()
 	router.Run(":" + configuration.Server.Port)
